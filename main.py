4a1135a751ac48204cf214cf81b42a372510ff71
clhandy16 <68276415+clhandy16@users.noreply.github.com>
2022-10-14 19:25:09 -0400

    Add core functionality
    -add payment processing, resource adjustment, validation checks
    -code cleanup

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# TODO: 1. print report of all coffee machine resources
-def runreport():
-    print("Water: " + str(resources["water"]) + "ml")
-    print("Milk: " + str(resources["milk"]) + "ml")
-    print("Coffee: " + str(resources["coffee"]) + "g")
-    print("Money: $" + str(resources["money"]))
-
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -39,48 +31,73 @@ resources = {
     "money": 0.0
 }
 
+def runreport():
+    print("Water: " + str(resources["water"]) + "ml")
+    print("Milk: " + str(resources["milk"]) + "ml")
+    print("Coffee: " + str(resources["coffee"]) + "g")
+    print("Money: $" + str(resources["money"]))
 
-# TODO: 2. prompt user for drink order
 def check_menu(drink):
-    drink_on_menu = False
     for item in MENU:
         if drink == item:
-            drink_on_menu = True
-            collect_coins()
+            return MENU[item]
             break
-    if not drink_on_menu:
-        new_drink = input("Sorry, we only serve espresso, latte or cappuccino. Which would you like?: ")
-        check_menu(new_drink)
+        else:
+            new_drink = input("Sorry, we only serve espresso, latte or cappuccino. Which would you like?: ")
+            check_menu(new_drink)
 
 
 # TODO: 3. check if resources sufficient to make drink order
 
 # got a start on building the logic for comparing ordered_drink dictionary to resources dictionary
 # probably refactor input script to copy relevant drink dictionary from MENU dictionary before expanding
-"""
-print(MENU[drink]["ingredients"])
 
-ingredients = MENU[drink]["ingredients"]
-for item in ingredients:
-  print(item + ": " + str(ingredients[item]))
-"""
+def check_resources(ingredients):
+    for item in ingredients:
+        if resources[item] < ingredients[item]:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True
 
 # TODO: 4. process payment
 
-def collect_coins():
-    q = float(input("Insert quarters: "))
-    d = float(input("Insert dimes: "))
-    n = float(input("Insert nickles: "))
-    p = float(input("Insert pennies: "))
-    count_coins(q, d, n, p)
+def process_coins():
+    print("Please insert coins.")
+    total = float(input("Insert quarters: ")) * 0.25
+    total += float(input("Insert dimes: ")) * 0.10
+    total += float(input("Insert nickles: ")) * 0.05
+    total += float(input("Insert pennies: ")) * 0.01
+    return total
 
-def count_coins(quarters, dimes, nickles, pennies):
-    resources["money"] += quarters * 0.25
-    resources["money"] += dimes * 0.10
-    resources["money"] += nickles * 0.05
-    resources["money"] += pennies * 0.01
 
-# TODO: 5. adjust resources before dispensing coffee
+def is_payment_sufficient(pmt, drink_cost):
+    if pmt >= drink_cost:
+        change = round(pmt - drink_cost, 2)
+        resources["money"] += round(drink_cost, 2)
+        print(f"Thank you! Your change is ${change}.")
+        return True
+    else:
+        print("Sorry, insufficient funds. Refunding payment.")
+        return False
 
-ordered_drink = input("What would you like? (espresso, latte, cappuccino): ")
-check_menu(ordered_drink)
+
+def make_coffee(drink_name, ingredients):
+    for item in ingredients:
+        resources[item] -= ingredients[item]
+    print(f"Here is your {drink_name}! Enjoy!")
+
+is_on = True
+
+while is_on:
+    ordered_drink = input("What would you like? (espresso, latte, cappuccino): ")
+    if ordered_drink == "off":
+        print("The coffee machine has been turned off.")
+        is_on = False
+    elif ordered_drink == "report":
+        runreport()
+    else:
+        drink_info = MENU[ordered_drink]
+        if check_resources(drink_info["ingredients"]):
+            payment = process_coins()
+            if is_payment_sufficient(payment, drink_info["cost"]):
+                make_coffee(ordered_drink, drink_info["ingredients"])
